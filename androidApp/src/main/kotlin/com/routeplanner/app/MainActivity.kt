763040799d5406c1ee90f241a816285e6f266cbe
IvanDev <ivancraco@hotmail.com>
2026-06-24 23:04:43 -0300

    se agregó el entry point

--- androidApp/src/main/kotlin/com/routeplanner/app/MainActivity.kt
+++ androidApp/src/main/kotlin/com/routeplanner/app/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.routeplanner.app.features.notifier.presentation.RoutePlannerScreen
+import com.routeplanner.app.features.notifier.presentation.previewRoute
+import com.routeplanner.app.ui.RoutePlannerTheme
 
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +14,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
+            RoutePlannerTheme {
+                App()
+            }
         }
     }
 }

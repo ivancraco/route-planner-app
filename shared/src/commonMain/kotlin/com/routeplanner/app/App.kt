a43d9e664bfdd3c8efd44c804a2a2252ad23386d
IvanDev <ivancraco@hotmail.com>
2026-06-24 23:00:01 -0300

    entry point de ambas plataformas

---
+++ shared/src/commonMain/kotlin/com/routeplanner/app/App.kt
@@ -0,0 +1,9 @@
+package com.routeplanner.app
+
+import androidx.compose.runtime.Composable
+import com.routeplanner.app.features.notifier.presentation.navigation.NavigationWrapper
+
+@Composable
+fun App() {
+    NavigationWrapper()
+}

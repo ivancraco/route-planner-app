97a28e0ff9b0e272faf85007b98e2b5255afc429
IvanDev <ivancraco@hotmail.com>
2026-07-09 18:12:31 -0300

    se optimizaron dependencias

--- androidApp/src/main/kotlin/com/routeplanner/app/MainActivity.kt
+++ androidApp/src/main/kotlin/com/routeplanner/app/MainActivity.kt
@@ -4,8 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.routeplanner.app.features.notifier.presentation.RoutePlannerScreen
-import com.routeplanner.app.features.notifier.presentation.previewRoute
 import com.routeplanner.app.ui.RoutePlannerTheme
 
 

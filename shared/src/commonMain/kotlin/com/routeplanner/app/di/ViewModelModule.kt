4617920f4085a91df681939f6437dacd7a0fdae0
IvanDev <ivancraco@hotmail.com>
2026-07-09 18:09:48 -0300

    inyección de dependencias para los viewModels

--- shared/src/commonMain/kotlin/com/routeplanner/app/di/ViewModelModule.kt
+++ shared/src/commonMain/kotlin/com/routeplanner/app/di/ViewModelModule.kt
@@ -3,9 +3,13 @@ package com.routeplanner.app.di
 import com.routeplanner.app.features.notifier.presentation.NotifierViewModel
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import kotlin.text.get
 
 fun viewModelModule() = module {
     viewModel {
-        NotifierViewModel(get())
+        NotifierViewModel(
+            notifierRepository = get(),
+            repository = get(),
+            locationBiasProvider = { null })
     }
 }

197e9868d5c88d2bbe614a72fcac58efd069cdc8
IvanDev <ivancraco@hotmail.com>
2026-07-13 17:38:50 -0300

    RoutePlannerTheme correctamente configurado

--- androidApp/src/main/kotlin/com/routeplanner/app/MainActivity.kt
+++ androidApp/src/main/kotlin/com/routeplanner/app/MainActivity.kt
@@ -4,17 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.routeplanner.app.ui.RoutePlannerTheme
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            RoutePlannerTheme {
-                App()
-            }
+            App()
         }
     }
 }
--- shared/src/commonMain/kotlin/com/routeplanner/app/App.kt
+++ shared/src/commonMain/kotlin/com/routeplanner/app/App.kt
@@ -2,8 +2,11 @@ package com.routeplanner.app
 
 import androidx.compose.runtime.Composable
 import com.routeplanner.app.features.notifier.presentation.navigation.NavigationWrapper
+import com.routeplanner.app.ui.RoutePlannerTheme
 
 @Composable
 fun App() {
-    NavigationWrapper()
+    RoutePlannerTheme {
+        NavigationWrapper()
+    }
 }
